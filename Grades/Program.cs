3faa90c0bca5b713dadad66c0e259472b29ba5c2
bryan kelly <bryankelly0077@gmail.com>
2018-10-23 11:10:21 +0100

    Add project files.

---
+++ Grades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(34);
+            book.AddGrade(23.4f);
+
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(64);
+        }
+    }
+}
